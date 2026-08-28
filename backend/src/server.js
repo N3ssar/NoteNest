d8e490a00594434769ee634e187197b37420a733
N3ssar <na55ar.swe@gmail.com>
2026-08-28 12:42:24 +0300

    fix: resolve route conflicts, update vercel rewrites, and clean backend static serving

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,15 +23,15 @@ if (process.env.NODE_ENV !== "production") {
 app.use(rateLimiter);
 app.use(express.json());
 
-app.use("/notes", notesRoutes);
+app.use("/api/notes", notesRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-  );
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+//   );
+// }
 
 const PORT = process.env.PORT || 5001;
 
